0ff7da33776f08b6c394f4d762a87e2c3ec193f7
Gonzalo Gluzman <gonzalo.gluzman@stensul.com>
2020-12-10 17:41:35 -0300

    Adding test cases for _Promise's catch

--- tests/catch.test.ts
+++ tests/catch.test.ts
@@ -4,6 +4,8 @@ const _pStringBoolean: _Promise<string, boolean> = null as any;
 _pStringBoolean; // $ExpectType _Promise<string, boolean>
 const _pNumber: _Promise<number, never> = null as any;
 _pNumber; // $ExpectType _Promise<number, never>
+const _pNumberString: _Promise<number, string> = null as any;
+_pNumberString; // $ExpectType _Promise<number, string>
 const _pRejectedNumber: _Promise<never, number> = null as any;
 _pRejectedNumber; // $ExpectType _Promise<never, number>
 
@@ -20,7 +22,13 @@ _pStringBoolean.catch(null); // $ExpectType _Promise<string, boolean>
 _pStringBoolean.catch(_ => 3); // $ExpectType _Promise<string | number, never>
 
 // with a callback returning a resolved PromiseLike
+_pStringBoolean.catch(_ => Promise.resolve(3));
+
+// with a callback returning an only resolved _Promise
 _pStringBoolean.catch(_ => _pNumber); // $ExpectType _Promise<string | number, never>
 
-// with a callback returning a rejected PromiseLike
+// with a callback returning a resolved/rejected _Promise
+_pStringBoolean.catch(_ => _pNumberString); // $ExpectType _Promise<string | number, string>
+
+// with a callback returning an only rejected PromiseLike
 _pStringBoolean.catch(_ => _pRejectedNumber); // $ExpectType _Promise<string, number>
