942c35be3b8d7418da7e668ecd94aa86073ef478
Gonzalo Gluzman <gonzalo.gluzman@stensul.com>
2020-12-10 17:09:12 -0300

    Adding test cases where a callback returns a _Promise

--- tests/catch.test.ts
+++ tests/catch.test.ts
@@ -2,6 +2,10 @@ import { _Promise } from '../src/es5';
 
 const _pString: _Promise<string> = null as any;
 _pString; // $ExpectType _Promise<string>
+const _pNumber: _Promise<number> = null as any;
+_pNumber; // $ExpectType _Promise<number>
+const _pRejectedNumber: _Promise<never> = null as any;
+_pRejectedNumber; // $ExpectType _Promise<never>
 
 // Without parameter
 _pString.catch(); // $ExpectType _Promise<string>
@@ -16,7 +20,7 @@ _pString.catch(null); // $ExpectType _Promise<string>
 _pString.catch(_ => 3); // $ExpectType _Promise<string | number>
 
 // with a callback returning a resolved PromiseLike
-_pString.catch(_ => Promise.resolve(3)); // $ExpectType _Promise<string | number>
+_pString.catch(_ => _pNumber); // $ExpectType _Promise<string | number>
 
 // with a callback returning a rejected PromiseLike
-_pString.catch(_ => Promise.reject(3)); // $ExpectType _Promise<string>
+_pString.catch(_ => _pRejectedNumber); // $ExpectType _Promise<string>
--- tests/then.test.ts
+++ tests/then.test.ts
@@ -2,6 +2,14 @@ import { _Promise } from '../src/es5';
 
 const _pString: _Promise<string> = null as any;
 _pString; // $ExpectType _Promise<string>
+const _pNumber: _Promise<number> = null as any;
+_pNumber; // $ExpectType _Promise<number>
+const _pBoolean: _Promise<boolean> = null as any;
+_pBoolean; // $ExpectType _Promise<boolean>
+const _pRejectedNumber: _Promise<never> = null as any;
+_pRejectedNumber; // $ExpectType _Promise<never>
+const _pRejectedBoolean: _Promise<never> = null as any;
+_pRejectedBoolean; // $ExpectType _Promise<never>
 
 // Without parameter
 _pString.then(); // $ExpectType _Promise<string>
@@ -21,6 +29,12 @@ _pString.then(_ => Promise.resolve(3)); // $ExpectType _Promise<number>
 // with a callback returning a rejected PromiseLike
 _pString.then(_ => Promise.reject(3)); // $ExpectType _Promise<never>
 
+// with a callback returning a resolved _Promise
+_pString.then(_ => _pNumber); // $ExpectType _Promise<number>
+
+// with a callback returning a rejected _Promise
+_pString.then(_ => _pRejectedNumber); // $ExpectType _Promise<never>
+
 /**
  * Passing a second argument
  */
@@ -38,6 +52,10 @@ _pString.then(undefined, _ => 3); // $ExpectType _Promise<string | number>
 _pString.then(undefined, _ => Promise.resolve(3)); // $ExpectType _Promise<string | number>
 // with a callback returning a rejected PromiseLike
 _pString.then(undefined, _ => Promise.reject(3)); // $ExpectType _Promise<string>
+// with a callback returning a resolved _Promise
+_pString.then(undefined, _ => _pNumber); // $ExpectType _Promise<string | number>
+// with a callback returning a rejected _Promise
+_pString.then(undefined, _ => _pRejectedNumber); // $ExpectType _Promise<string>
 
 /**
  * First argument null
@@ -52,6 +70,10 @@ _pString.then(null, _ => 3); // $ExpectType _Promise<string | number>
 _pString.then(null, _ => Promise.resolve(3)); // $ExpectType _Promise<string | number>
 // with a callback returning a rejected PromiseLike
 _pString.then(null, _ => Promise.reject(3)); // $ExpectType _Promise<string>
+// with a callback returning a resolved _Promise
+_pString.then(null, _ => _pNumber); // $ExpectType _Promise<string | number>
+// with a callback returning a rejected PromiseLike
+_pString.then(null, _ => _pRejectedNumber); // $ExpectType _Promise<string>
 
 /**
  * First argument is a callback returning plain value
@@ -66,6 +88,10 @@ _pString.then(_ => 3, _ => true); // $ExpectType _Promise<number | boolean>
 _pString.then(_ => 3, _ => Promise.resolve(true)); // $ExpectType _Promise<number | boolean>
 // with a callback returning a rejected PromiseLike
 _pString.then(_ => 3, _ => Promise.reject(true)); // $ExpectType _Promise<number>
+// with a callback returning a resolved _Promise
+_pString.then(_ => 3, _ => _pBoolean); // $ExpectType _Promise<number | boolean>
+// with a callback returning a rejected _Promise
+_pString.then(_ => 3, _ => _pRejectedBoolean); // $ExpectType _Promise<number>
 
 /**
  * First argument is a callback returning resolved PromiseLike
@@ -80,6 +106,10 @@ _pString.then(_ => Promise.resolve(3), _ => true); // $ExpectType _Promise<numbe
 _pString.then(_ => Promise.resolve(3), _ => Promise.resolve(true)); // $ExpectType _Promise<number | boolean>
 // with a callback returning a rejected PromiseLike
 _pString.then(_ => Promise.resolve(3), _ => Promise.reject(true)); // $ExpectType _Promise<number>
+// with a callback returning a resolved _Promise
+_pString.then(_ => Promise.resolve(3), _ => _pBoolean); // $ExpectType _Promise<number | boolean>
+// with a callback returning a rejected _Promise
+_pString.then(_ => Promise.resolve(3), _ => _pRejectedBoolean); // $ExpectType _Promise<number>
 
 /**
  * First argument is a callback returning rejected PromiseLike
@@ -94,3 +124,7 @@ _pString.then(_ => Promise.reject(3), _ => true); // $ExpectType _Promise<boolea
 _pString.then(_ => Promise.reject(3), _ => Promise.resolve(true)); // $ExpectType _Promise<boolean>
 // with a callback returning a rejected PromiseLike
 _pString.then(_ => Promise.reject(3), _ => Promise.reject(true)); // $ExpectType _Promise<never>
+// with a callback returning a resolved _Promise
+_pString.then(_ => Promise.reject(3), _ => _pBoolean); // $ExpectType _Promise<boolean>
+// with a callback returning a rejected _Promise
+_pString.then(_ => Promise.reject(3), _ => _pRejectedBoolean); // $ExpectType _Promise<never>
