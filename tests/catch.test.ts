034bdfad45c3841ede9fccab2b9beafafdc55927
Gonzalo Gluzman <gonzalo.gluzman@stensul.com>
2020-12-11 01:32:17 -0300

    Tests: added rejected value tests for _Promise's catch

--- tests/catch.test.ts
+++ tests/catch.test.ts
@@ -19,31 +19,37 @@ _pStringBoolean.catch(undefined); // $ExpectType _Promise<string, boolean>
 _pStringBoolean.catch(null); // $ExpectType _Promise<string, boolean>
 
 // with a callback returning a plain value
-_pStringBoolean.catch(_ => { // $ExpectType _Promise<string | number, unknownError>
+_pStringBoolean.catch(e => { // $ExpectType _Promise<string | number, unknownError>
+    e; // $ExpectType boolean
     return 3;
 });
 
 // with a callback returning a resolved PromiseLike
-_pStringBoolean.catch(_ => { // $ExpectType _Promise<string | number, unknownError>
+_pStringBoolean.catch(e => { // $ExpectType _Promise<string | number, unknownError>
+    e; // $ExpectType boolean
     return Promise.resolve(3);
 });
 
 // with a callback returning a rejected PromiseLike
-_pStringBoolean.catch(_ => { // $ExpectType _Promise<string, unknownError>
+_pStringBoolean.catch(e => { // $ExpectType _Promise<string, unknownError>
+    e; // $ExpectType boolean
     return Promise.reject(3);
 });
 
 // with a callback returning an only resolved _Promise
-_pStringBoolean.catch(_ => { // $ExpectType _Promise<string | number, unknownError>
+_pStringBoolean.catch(e => { // $ExpectType _Promise<string | number, unknownError>
+    e; // $ExpectType boolean
     return _pNumber;
 });
 
 // with a callback returning a resolved/rejected _Promise
-_pStringBoolean.catch(_ => { // $ExpectType _Promise<string | number, string | unknownError>
+_pStringBoolean.catch(e => { // $ExpectType _Promise<string | number, string | unknownError>
+    e; // $ExpectType boolean
     return _pNumberString;
 });
 
 // with a callback returning an only rejected PromiseLike
-_pStringBoolean.catch(_ => { // $ExpectType _Promise<string, number | unknownError>
+_pStringBoolean.catch(e => { // $ExpectType _Promise<string, number | unknownError>
+    e; // $ExpectType boolean
     return _pRejectedNumber;
 });
