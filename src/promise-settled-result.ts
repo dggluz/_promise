87c7b7e2e6866511b1e1272b9b4ff1324604df92
Gonzalo Gluzman <gonzalo.gluzman@stensul.com>
2020-12-15 02:46:58 -0300

    Fixing building errors

--- src/promise-settled-result.ts
+++ src/promise-settled-result.ts
@@ -1,6 +1,11 @@
+interface _PromiseFulfilledResult<T> {
+    status: "fulfilled";
+    value: T;
+}
+
 export interface _PromiseRejectedResult<E> {
     status: "rejected";
     reason: E;
 }
 
-export type _PromiseSettledResult<T, E> = PromiseFulfilledResult<T> | _PromiseRejectedResult<E>;
+export type _PromiseSettledResult<T, E> = _PromiseFulfilledResult<T> | _PromiseRejectedResult<E>;
